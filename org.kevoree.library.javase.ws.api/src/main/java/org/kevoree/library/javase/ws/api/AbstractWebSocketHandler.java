8dc61d9d7c4a28a6fb838f8486bdabdddcf201fe
Erwan Daubert <erwan.daubert@gmail.com>
2013-07-29 10:33:33 +0200

    add project to WebSocket Server and handlers (empty components)

---
+++ org.kevoree.library.javase.ws.api/src/main/java/org/kevoree/library/javase/ws/api/AbstractWebSocketHandler.java
@@ -0,0 +1,12 @@
+package org.kevoree.library.javase.ws.api;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 29/07/13
+ * Time: 10:07
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+public class AbstractWebSocketHandler {
+}
