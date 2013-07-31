f977123e8471f92c7311c99b7345e46b83120f2a
Erwan Daubert <erwan.daubert@gmail.com>
2013-07-31 17:48:45 +0200

    change method visibilirefactor Helper

---
+++ org.kevoree.library.javase.http.api/src/main/java/org/kevoree/library/javase/http/api/HTTPHelper.java
@@ -0,0 +1,55 @@
+package org.kevoree.library.javase.http.api;
+
+import org.kevoree.log.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 31/07/13
+ * Time: 17:36
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+public class HTTPHelper {
+    private static Properties mime = initMime();
+
+    protected static Properties initMime() {
+        Properties p = new Properties();
+        try {
+            p.load(StaticFileHandlerHelper.class.getClassLoader().getResourceAsStream("mime.properties"));
+        } catch (Exception ex) {
+            Log.debug("MIME map can't be loaded:" + ex);
+        }
+        return p;
+    }
+
+    public static boolean isRaw(String url) {
+        Log.debug("look extension file to know if the file '{}' is a raw file", url);
+        return !(url.endsWith(".js") || url.endsWith(".html") || url.endsWith(".css") || url.endsWith(".jnlp"));
+    }
+
+    public static String getHttpHeaderFromURL(String url) {
+        int dp = url.lastIndexOf('.');
+        if (dp > 0) {
+            return mime.getProperty(url.substring(dp + 1).toUpperCase());
+        }
+        return "text/html";
+    }
+
+    public static byte[] convertStream(InputStream in) throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int l;
+        do {
+            l = (in.read(buffer));
+            if (l > 0) {
+                out.write(buffer, 0, l);
+            }
+        } while (l > 0);
+        return out.toByteArray();
+    }
+}
--- org.kevoree.library.javase.http.api/src/main/java/org/kevoree/library/javase/http/api/StaticFileHandlerHelper.java
+++ org.kevoree.library.javase.http.api/src/main/java/org/kevoree/library/javase/http/api/StaticFileHandlerHelper.java
@@ -5,7 +5,6 @@ import org.kevoree.log.Log;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.util.Properties;
 
 /**
  * User: Erwan Daubert - erwan.daubert@gmail.com
@@ -16,20 +15,6 @@ import java.util.Properties;
  * @version 1.0
  */
 public class StaticFileHandlerHelper {
-
-
-    private static Properties mime = initMime();
-
-    protected static Properties initMime() {
-        Properties p = new Properties();
-        try {
-            p.load(StaticFileHandlerHelper.class.getClassLoader().getResourceAsStream("mime.properties"));
-        } catch (Exception ex) {
-            Log.debug("MIME map can't be loaded:" + ex);
-        }
-        return p;
-    }
-
 
     public static boolean checkStaticFileFromDir(String defaultFile, String baseDir, AbstractHTTPHandler origin, HttpServletRequest req, HttpServletResponse resp) {
         String filePath = req.getRequestURI();
@@ -55,16 +40,16 @@ public class StaticFileHandlerHelper {
             try {
                 FileInputStream ins = new FileInputStream(in);
 
-                if (isRaw(filePath)) {
+                if (HTTPHelper.isRaw(filePath)) {
                     OutputStream writer = resp.getOutputStream();
-                    writer.write(convertStream(ins));
+                    writer.write(HTTPHelper.convertStream(ins));
                     writer.flush();
                 } else {
                     PrintWriter writer = resp.getWriter();
-                    writer.println(new String(convertStream(ins), "UTF-8"));
+                    writer.println(new String(HTTPHelper.convertStream(ins), "UTF-8"));
                     writer.flush();
                 }
-                resp.addHeader("Content-Type", (getHttpHeaderFromURL(filePath)));
+                resp.addHeader("Content-Type", (HTTPHelper.getHttpHeaderFromURL(filePath)));
 
                 ins.close();
                 return true;
@@ -86,16 +71,16 @@ public class StaticFileHandlerHelper {
             try {
                 FileInputStream ins = new FileInputStream(in);
 
-                if (isRaw(filePath)) {
+                if (HTTPHelper.isRaw(filePath)) {
                     OutputStream writer = resp.getOutputStream();
-                    writer.write(convertStream(ins));
+                    writer.write(HTTPHelper.convertStream(ins));
                     writer.flush();
                 } else {
                     PrintWriter writer = resp.getWriter();
-                    writer.println(new String(convertStream(ins), "UTF-8"));
+                    writer.println(new String(HTTPHelper.convertStream(ins), "UTF-8"));
                     writer.flush();
                 }
-                resp.addHeader("Content-Type", (getHttpHeaderFromURL(filePath)));
+                resp.addHeader("Content-Type", (HTTPHelper.getHttpHeaderFromURL(filePath)));
 
                 ins.close();
                 return true;
@@ -130,16 +115,16 @@ public class StaticFileHandlerHelper {
         InputStream in = origin.getClass().getClassLoader().getResourceAsStream(filePath);
         if (in != null) {
             try {
-                if (isRaw(filePath)) {
+                if (HTTPHelper.isRaw(filePath)) {
                     OutputStream writer = resp.getOutputStream();
-                    writer.write(convertStream(in));
+                    writer.write(HTTPHelper.convertStream(in));
                     writer.flush();
                 } else {
                     PrintWriter writer = resp.getWriter();
-                    writer.println(new String(convertStream(in), "UTF-8"));
+                    writer.println(new String(HTTPHelper.convertStream(in), "UTF-8"));
                     writer.flush();
                 }
-                resp.addHeader("Content-Type", (getHttpHeaderFromURL(filePath)));
+                resp.addHeader("Content-Type", (HTTPHelper.getHttpHeaderFromURL(filePath)));
                 return true;
             } catch (Exception e) {
                 Log.error("", e);
@@ -155,16 +140,16 @@ public class StaticFileHandlerHelper {
         InputStream in = origin.getClass().getClassLoader().getResourceAsStream(filePath);
         if (in != null) {
             try {
-                if (isRaw(filePath)) {
+                if (HTTPHelper.isRaw(filePath)) {
                     OutputStream writer = resp.getOutputStream();
-                    writer.write(convertStream(in));
+                    writer.write(HTTPHelper.convertStream(in));
                     writer.flush();
                 } else {
                     PrintWriter writer = resp.getWriter();
-                    writer.println(new String(convertStream(in), "UTF-8"));
+                    writer.println(new String(HTTPHelper.convertStream(in), "UTF-8"));
                     writer.flush();
                 }
-                resp.addHeader("Content-Type", (getHttpHeaderFromURL(filePath)));
+                resp.addHeader("Content-Type", (HTTPHelper.getHttpHeaderFromURL(filePath)));
                 return true;
             } catch (Exception e) {
                 Log.error("", e);
@@ -173,31 +158,5 @@ public class StaticFileHandlerHelper {
             Log.debug("Resource {} doesn't exist ", filePath);
         }
         return false;
-    }
-
-    private static boolean isRaw(String url) {
-        Log.debug("look extension file to know if the file '{}' is a raw file", url);
-        return !(url.endsWith(".js") || url.endsWith(".html") || url.endsWith(".css") || url.endsWith(".jnlp"));
-    }
-
-    private static String getHttpHeaderFromURL(String url) {
-        int dp = url.lastIndexOf('.');
-        if (dp > 0) {
-            return mime.getProperty(url.substring(dp + 1).toUpperCase());
-        }
-        return "text/html";
-    }
-
-    public static byte[] convertStream(InputStream in) throws Exception {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int l;
-        do {
-            l = (in.read(buffer));
-            if (l > 0) {
-                out.write(buffer, 0, l);
-            }
-        } while (l > 0);
-        return out.toByteArray();
     }
 }
